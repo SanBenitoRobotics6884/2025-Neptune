7c3eb0ca69b3508441e1ad6dbf149ca9873efafc
Voyager6884 <dsr6884voyager@gmail.com>
2025-02-04 15:38:53 -0800

    climb

---
+++ src/main/java/frc/robot/ClimbSubsystem.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimbSubsystem extends SubsystemBase {
+  TalonFX m_climbMotor;
+  TalonFXConfigurator configurator;
+  Encoder encoder;
+  PIDController pidController;
+  /** Creates a new ClimbSubsystem. */
+  public ClimbSubsystem() {
+    m_climbMotor = new TalonFX(0);
+    m_climbMotor.optimizeBusUtilization();
+    encoder = new Encoder(0, 0);
+    pidController = new PIDController(0, 0, 0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+  
+  XboxController m_XboxController = new XboxController(0);
+ClimbCommand m_ClimbCommand = new ClimbCommand();
+ClimbCommandUp m_ClimbCommandUp = new ClimbCommandUp();
+ClimbStop m_ClimbStop = new ClimbStop();
+  public RobotContainer() {
+    configureBindings();
+if(m_XboxController.getBButtonPressed()){
+  m_ClimbCommand.schedule();
+  
+}
+if (m_XboxController.getAButtonPressed()) {
+  m_ClimbStop.schedule();
+}
+if (m_XboxController.getLeftBumperButtonPressed()) {
+  m_ClimbCommandUp.schedule();
+}
+
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
