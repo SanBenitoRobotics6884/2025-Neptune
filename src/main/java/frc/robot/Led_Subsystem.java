069aeef00b684786035a1a9b516ed057efb886fa
Voyager6884 <dsr6884voyager@gmail.com>
2025-02-21 12:34:10 -0800

    change to array for ease

--- src/main/java/frc/robot/Led_Subsystem.java
+++ src/main/java/frc/robot/Led_Subsystem.java
@@ -4,38 +4,49 @@
 
 package frc.robot;
 
+import java.util.Optional;
+
+import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+// Hawk Tuah | Led Code
 public class Led_Subsystem extends SubsystemBase {
-//hawk tuah
-  public Led_Subsystem() {}
-AddressableLED m_led = new AddressableLED(9);
-AddressableLEDBuffer m_LedBuffer = new AddressableLEDBuffer(60);
+  AddressableLED m_led = new AddressableLED(9);
+  AddressableLEDBuffer m_LedBuffer = new AddressableLEDBuffer(60);
+
+    LEDPattern[] patterns = {
+      LEDPattern.solid(Color.kRed), //Alliance Red
+      LEDPattern.solid(Color.kBlue), // Alliance Blue
+      LEDPattern.solid(Color.kGreen), // Deep Cage
+      LEDPattern.solid(Color.kAquamarine), //Elevator
+      LEDPattern.solid(Color.kHotPink), //Algae
+      LEDPattern.solid(Color.kYellow), // Coral
+      LEDPattern.solid(Color.kDarkOrange) // PIT MODE
+    };
+
+    Optional<Alliance> m_alliance = DriverStation.getAlliance();
+
+  public Led_Subsystem() {
+    
+  }
 
   @Override
   public void periodic() {
-    m_led.setLength(m_LedBuffer.getLength());
-    m_led.setData(m_LedBuffer);
+    m_led.setLength(m_LedBuffer.getLength());    m_led.setData(m_LedBuffer);
     m_led.start();
 
-    LEDPattern Alliance_red = LEDPattern.solid(Color.kRed);
-    LEDPattern Alliance_blue = LEDPattern.solid(Color.kBlue);
-    LEDPattern DeepCage = LEDPattern.solid(Color.kGreen);
-    LEDPattern elavatorPattern = LEDPattern.solid(Color.kAquamarine );
-    LEDPattern algae = LEDPattern.solid(Color.kHotPink);
-    LEDPattern coral = LEDPattern.solid(Color.kYellow);
-    LEDPattern Pit_mode = LEDPattern.solid(Color.kDarkOrange);
-
-    Alliance_blue.applyTo(m_LedBuffer);
-    Alliance_red.applyTo(m_LedBuffer);
-    DeepCage.applyTo(m_LedBuffer);
-    elavatorPattern.applyTo(m_LedBuffer);
-    algae.applyTo(m_LedBuffer);
-    coral.applyTo(m_LedBuffer);
-    Pit_mode.applyTo(m_LedBuffer);
+    patterns[0].applyTo(m_LedBuffer);
+    patterns[1].applyTo(m_LedBuffer);
+    patterns[2].applyTo(m_LedBuffer);
+    patterns[3].applyTo(m_LedBuffer);
+    patterns[4].applyTo(m_LedBuffer);
+    patterns[5].applyTo(m_LedBuffer);
+    patterns[6].applyTo(m_LedBuffer);
+    //* MAKE SURE THAT THE SPECIFIC PATTERNS CORRELATE TO THE SITUATION */
   }
 }
