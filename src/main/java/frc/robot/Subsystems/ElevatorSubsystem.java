46ce9f181fee0b9a4f5e5f14a7926af18455cdbc
nesan <nwaran@gmail.com>
2025-02-26 23:13:14 -0800

    publish climb/elevator kraken positions to dashboard

--- src/main/java/frc/robot/Subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ElevatorSubsystem.java
@@ -7,8 +7,10 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.AbsoluteEncoder;
 
+import static edu.wpi.first.units.Units.Degrees;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.math.Conversions;
 import frc.robot.Constants;
@@ -47,6 +49,7 @@ public class ElevatorSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Elevator Motor Rot ", mMotor.getPosition().getValue().in(Degrees));
     pidController.atSetpoint();
     if (xboxController.getLeftBumperButtonPressed()) {
       setSetpoit(0);
