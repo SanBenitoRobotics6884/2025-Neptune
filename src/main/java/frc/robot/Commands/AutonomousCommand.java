04e64154416e9f724f7e2cbc33949c3ffa9f7d40
Voyager6884 <dsr6884voyager@gmail.com>
2025-02-27 16:52:28 -0800

    test

--- src/main/java/frc/robot/Commands/AutonomousCommand.java
+++ src/main/java/frc/robot/Commands/AutonomousCommand.java
@@ -2,11 +2,12 @@ package frc.robot.Commands;
 import frc.robot.Subsystems.Swerve;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class AutonomousCommand  extends Command {
+public class AutonomousCommand extends Command {
     private final Swerve m_swerve;
-
+    SmartDashboard m_smartdashboard;
     public AutonomousCommand(Swerve s_Swerve){
         addRequirements(s_Swerve);
         m_swerve = s_Swerve;
@@ -16,6 +17,7 @@ public class AutonomousCommand  extends Command {
     }
 
     public void autonomousPeriodic() {
+        m_smartdashboard.putData(m_swerve);
         double targetSpeed = 2.0/*meters*/ / 30/*seconds*/;
         this.m_swerve.drive(
             new Translation2d(-1, 0).times(targetSpeed),
@@ -23,6 +25,6 @@ public class AutonomousCommand  extends Command {
             true,
             true
         );
-        
+
     }
 }
