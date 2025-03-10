9c220d787a59c067467423fc2302f031fc9e8ac9
nesan <nwaran@gmail.com>
2025-03-10 09:22:10 -0700

    Example test class

---
+++ src/test/java/TestSwerve.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import frc.robot.Constants;
+import frc.robot.SwerveMod;
+
+class TestSwerve {
+    public static void main(String[] args) {
+    }
+
+    @Test
+    void testInitialization(){
+        SwerveMod mod = new SwerveMod(1, Constants.Swerve.Mod0.constants);
+        assertEquals(1, mod.moduleNumber);        
+    }
+
+}
