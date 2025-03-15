26d231c563b6b1cf33fd75f06582c2d8dcb520b6
Macbook <sbhs6884@gmail.com>
2025-03-14 19:02:59 -0700

    arducam class not finished

---
+++ src/main/java/frc/lib/util/Arducam.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.lib.util;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+/** Add your docs here. */
+public class Arducam {
+    PhotonCamera m_arduCamera = new PhotonCamera(null, "Front Arducam");
+    
+}
