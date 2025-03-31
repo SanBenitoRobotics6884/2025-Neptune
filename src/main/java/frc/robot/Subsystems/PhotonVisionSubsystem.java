071aefcc7638cafe6eba95353bf9579018f7d919
nesan <nwaran@gmail.com>
2025-03-31 16:23:41 -0700

    remove unnecessary debug statements

--- src/main/java/frc/robot/Subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/PhotonVisionSubsystem.java
@@ -1,34 +1,11 @@
 package frc.robot.Subsystems;
 
-import frc.robot.Constants;
-
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-//import com.ctre.phoenix.sensors.PigeonIMU;
-import com.ctre.phoenix6.configs.Pigeon2Configuration;
-import com.ctre.phoenix6.hardware.Pigeon2;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-import com.pathplanner.lib.util.PathPlannerLogging;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 
 public class PhotonVisionSubsystem extends SubsystemBase {
     PhotonCamera camera = new PhotonCamera("arducam-6884");
@@ -40,11 +17,11 @@ public class PhotonVisionSubsystem extends SubsystemBase {
         PhotonPipelineResult result = camera.getLatestResult();
         if(result.hasTargets()){
             PhotonTrackedTarget target = result.getBestTarget();
-            SmartDashboard.putNumber("PV-yaw", target.getYaw());
-            SmartDashboard.putNumber("PV-pitch", target.getPitch());
-            SmartDashboard.putNumber("PV-area", target.getArea());
-            SmartDashboard.putNumber("PV-skew", target.getSkew());
-            SmartDashboard.putNumber("PV-val", target.fiducialId);
+            // SmartDashboard.putNumber("PV-yaw", target.getYaw());
+            // SmartDashboard.putNumber("PV-pitch", target.getPitch());
+            // SmartDashboard.putNumber("PV-area", target.getArea());
+            // SmartDashboard.putNumber("PV-skew", target.getSkew());
+            // SmartDashboard.putNumber("PV-val", target.fiducialId);
             return target.getYaw() / 45;
         }
         return 0.0;
@@ -52,15 +29,5 @@ public class PhotonVisionSubsystem extends SubsystemBase {
     
     @Override
     public void periodic(){
-        PhotonPipelineResult result = camera.getLatestResult();
-        if(result.hasTargets()){
-            PhotonTrackedTarget target = result.getBestTarget();
-            SmartDashboard.putNumber("PV-yaw", target.getYaw());
-            SmartDashboard.putNumber("PV-pitch", target.getPitch());
-            SmartDashboard.putNumber("PV-area", target.getArea());
-            SmartDashboard.putNumber("PV-skew", target.getSkew());
-            SmartDashboard.putNumber("PV-val", target.fiducialId);
-                
-        }
     }
 }
